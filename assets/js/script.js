a68a0d1738e24429d60ca27f5157941d1ef6b13f
Candice Hall <hall.candice@outlook.com>
2022-03-18 20:23:13 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,7 +149,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -185,16 +185,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
 
@@ -232,12 +234,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
     ui.draggable.remove();
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
 
@@ -256,6 +261,12 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days"))<=2){
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, 1800000);
 };
 
 $("#modalDueDate").datepicker({
